00a6b20e0336e83b82882ec694d9b2e413653dc1
hxuhack <china.xuhui@gmail.com>
2017-01-31 18:18:12 +0800

    update

---
+++ gitcommit.sh
@@ -0,0 +1,7 @@
+git add .
+git commit -m "update"
+git push origin master
+expect "Username for 'https://github.com':"
+send "hxuhack\r"
+expect "Password for 'https://github.com':"
+send "yurong0429\r"
