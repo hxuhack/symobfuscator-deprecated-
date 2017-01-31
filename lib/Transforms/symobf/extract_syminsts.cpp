69d8e7657a534c7009af15d08285e1536e434cdb
hxuhack <china.xuhui@gmail.com>
2017-01-31 18:12:32 +0800

    update

--- lib/Transforms/symobf/extract_syminsts.cpp
+++ lib/Transforms/symobf/extract_syminsts.cpp
@@ -1,41 +1,52 @@
-//===- Hello.cpp - Example code from "Writing an LLVM Pass" ---------------===//
-//
-//                     The LLVM Compiler Infrastructure
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
-//
-//===----------------------------------------------------------------------===//
-//
-// This file implements two versions of the LLVM "Hello World" pass described
-// in docs/WritingAnLLVMPass.html
-//
-//===----------------------------------------------------------------------===//
-
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 using namespace llvm;
 
 #define DEBUG_TYPE "Extract_SymInsts"
 
-STATISTIC(HelloCounter, "Counts number of functions greeted");
+
+const std::string default_symvar = "symvar";
+
+static cl::opt<std::string> symvar("symvar", cl::desc("choose a symbolic variable to obfuscate"), cl::value_desc("variable name"), cl::init(default_symvar), cl::Optional);
 
 namespace {
-  // Hello - The first implementation, without getAnalysisUsage.
-  struct extract_syminsts : public BasicBlockPass {
+  struct ExtSymInsts : public FunctionPass {
+    static char ID; 
+    ExtSymInsts() : FunctionPass(ID) {}
+    virtual bool runOnFunction(Function &F){
+      errs()<<symvar<<'\n';
+    }
+  };
+/*
+  struct extract_syminsts1 : public BasicBlockPass {
     static char ID; // Pass identification, replacement for typeid
-    extract_syminsts() : BasicBlockPass(ID) {}
+    extract_syminsts1() : BasicBlockPass(ID) {}
     virtual bool runOnBasicBlock(BasicBlock &BB) {
       for (BasicBlock::iterator it = BB.begin(); it != BB.end(); ++it){
-        errs()<<it->getName() << '\n';
+        //errs()<<it->getName() <<":"<<it->getOpcodeName()<<'\n';
+        it->print(errs());
+        errs()<<'\n';
       }
       return false;
     }
   };
+*/
+/*
+  struct extract_syminsts : public ImmutablePass {
+    static char ID; //
+    extract_syminsts() : ImmutablePass(ID) {}
+    virtual bool runOnModule(Module &MD) {
+      errs()<<MD.getNamedValue("str")<<'\n';
+      errs()<<symvar<<'\n';
+      return false;
+    }
+  };
+*/
 }
 
-char extract_syminsts::ID = 0;
-static RegisterPass<extract_syminsts> X("extract_syminsts", "extract symbolic instructions");
+char ExtSymInsts::ID = 0;
+static RegisterPass<ExtSymInsts> X("syminst", "extract symbolic instructions");
