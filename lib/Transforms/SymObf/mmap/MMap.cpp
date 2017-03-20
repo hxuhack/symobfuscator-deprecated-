3bfcb30e652ae32bd75a85e7a9c190a9c86be786
hxuhack <china.xuhui@gmail.com>
2017-03-20 15:45:45 +0800

    update

--- lib/Transforms/SymObf/mmap/MMap.cpp
+++ lib/Transforms/SymObf/mmap/MMap.cpp
@@ -3,100 +3,157 @@
 
 using namespace std;
 
-int64_t zMax = 8;
-int64_t hMax = 16;
-int64_t gMax = 2048;
-int64_t pMax = 2147483647;
+int64_t zLen, hLen, beta, gMax, alpha, pLen, gLen;
+
+mpz_t tmpRi;
 
 secparam sp;
+
+void GetMpzPrime(mpz_t prime, int64_t len){
+  mpz_init(prime);
+  gmp_randstate_t grt;  
+  gmp_randinit_default(grt); 
+  gmp_randseed_ui(grt, time(NULL));
+  mpz_urandomb(prime, grt, len); 
+  if(mpz_even_p(prime))  
+    mpz_add_ui(prime, prime, 1);
+  while(!mpz_probab_prime_p(prime, 25) > 0)
+    mpz_add_ui(prime, prime, 2);  
+}
 
 int64_t MMapInitParam(int64_t z, int64_t n, int64_t setnum){
   printf("Initializing MMap parameters...\n");
   sp.Z = z;
   sp.N = n;
   sp.setnum = setnum;
-  sp.z = (int64_t *) malloc (sizeof(int64_t) * sp.Z); 
-  sp.h = (int64_t *) malloc (sizeof(int64_t) * sp.N); 
-  sp.g = (int64_t *) malloc (sizeof(int64_t) * sp.N); 
-  sp.ginv = (int64_t *) malloc (sizeof(int64_t) * sp.N); 
-  sp.p = (int64_t *) malloc (sizeof(int64_t) * sp.N); 
+  sp.z = (mpz_t *) malloc (sizeof(mpz_t) * sp.Z); 
+  sp.h = (mpz_t *) malloc (sizeof(mpz_t) * sp.N); 
+  sp.g = (mpz_t *) malloc (sizeof(mpz_t) * sp.N); 
+  sp.ginv = (mpz_t *) malloc (sizeof(mpz_t) * sp.N); 
+  sp.p = (mpz_t *) malloc (sizeof(mpz_t) * sp.N); 
   sp.setid = (vector<int64_t> *) malloc (sizeof(vector<int64_t>) * sp.setnum); 
 
-  srand((unsigned)time(NULL));
+  zLen = 8;
+  hLen = 32;
+  gLen = 32;
+  pLen = 128;
+  sp.lambda = 32;
+  alpha = sp.lambda;
+  beta = sp.lambda;
+
+  gmp_randstate_t grt;  
+  gmp_randinit_default(grt); 
+  gmp_randseed_ui(grt, time(NULL));
+
   for(int64_t i =0; i< sp.Z; i++){
-    sp.z[i] = rand() % zMax + 1; //z cannot be 0; 
+	mpz_init(sp.z[i]); 
+    mpz_urandomb(sp.z[i], grt, zLen);//TODO:exclude 0; 
   }
 
+  mpz_init_set_ui(sp.q, 1);
   for(int64_t i =0; i< sp.N; i++){
-    sp.g[i] = GetPrime(gMax); 
-    sp.p[i] = GetPrime(pMax); 
-    sp.ginv[i] = InvMod(sp.g[i], sp.p[i]); 
-    sp.h[i] = rand() % hMax + 1; //h[1] cannot be 0; 
-    sp.q = sp.q * sp.p[i]; 
+    //sp.g[i] = GetPrime(gMax);
+	mpz_init(sp.g[i]); 
+	GetMpzPrime(sp.g[i], gLen); 
+	mpz_init(sp.p[i]); 
+	GetMpzPrime(sp.p[i], pLen); 
+    //sp.ginv[i] = InvMod(sp.g[i], sp.p[i]); 
+	mpz_init(sp.ginv[i]); 
+	mpz_invert(sp.ginv[i], sp.g[i], sp.p[i]);
+	mpz_init(sp.h[i]);
+    mpz_urandomb(sp.h[i], grt, hLen);//TODO:exclude 0; 
+    //sp.q = sp.q * sp.p[i]; 
+	mpz_mul(sp.q, sp.q, sp.p[i]);
   }
+
+  for(int64_t i=0; i<sp.N; i++){
+	mpz_t mid, tail;
+	mpz_init_set_ui(mid,1);
+	mpz_init_set_ui(tail,1);
+    for(int64_t j=0; j<sp.Z; j++){
+	  //mid = (mid * sp.ginv[i] * sp.z[j]) % sp.p[i] % sp.q;
+	  mpz_mul(mid,mid,sp.ginv[i]);
+	  mpz_mul(mid,mid,sp.z[j]);
+	  mpz_mod(mid,mid,sp.p[i]);
+	  mpz_mod(mid,mid,sp.q);
+	}
+    for(int64_t k=0; k<sp.N; k++){
+	  if (k!=i){
+	    //tail = tail * sp.p[k] % sp.q;
+	    mpz_mul(tail,tail,sp.p[k]);
+	    mpz_mod(tail,tail,sp.q);
+	  }
+	}
+    //sp.pzt = (sp.pzt + sp.h[i] * mid * tail)%sp.q; 
+	mpz_mul(mid,mid,sp.h[i]);
+	mpz_mul(mid,mid,tail);
+	mpz_add(sp.pzt,sp.pzt,mid);
+	mpz_mod(sp.pzt,sp.pzt,sp.q);
+  }
+
   //TODO:To implement more secure set mechanism
+  cout<<"pzt = " << sp.pzt<< endl;
   for(int64_t i =0; i< sp.setnum; i++){
-    sp.setid[i].push_back(sp.z[i]);
+    sp.setid[i].push_back(i);
   }
   printf("Finish MMap parameter initialization...\n");
   for(int64_t i =0; i< sp.N; i++){
-    printf("g[%d] = %d\n", i, sp.g[i]);
+    cout << "g[" <<i<<"]="<< sp.g[i]<<endl;
   }
   for(int64_t i =0; i< sp.N; i++){
-    printf("p[%d] = %d\n", i, sp.p[i]);
+    cout << "p[" <<i<<"]="<< sp.p[i]<<endl;
   }
-  printf("q = %d\n", sp.q);
+  cout<<"q = "<<sp.q<<endl;
   for(int64_t i =0; i< sp.Z; i++){
-    printf("z[%d] = %d\n", i, sp.z[i]);
+    cout << "z[" <<i<<"]="<< sp.z[i]<<endl;
   }
   return 0;
 }
 
-int64_t MMapIsZero(double u){
-  double pzt = 0;
-  for(int64_t i=0; i<sp.N; i++){
-	int mid = 1;
-	int64_t tail = 1;
-    for(int64_t j=0; j<sp.Z; j++){
-	  if(sp.p[i]==0){
-	    printf("!!!p[i]=0. Denominator cannot be 0!\n");
-	  }
-	  mid = (mid * sp.ginv[i] * sp.z[j]) % sp.p[i];
-	}
-    for(int64_t k=0; k<sp.N; k++){
-	  if (k!=i){
-	    tail = tail * sp.p[k] % sp.q;
-	  }
-	}
-    pzt = pzt + sp.h[i] * mid * tail; 
-  }
-  if(sp.q==0){
-    printf("!!!q=0. Denominator cannot be 0!\n");
-  }
-  pzt = fmod((pzt * u), sp.q);
-  printf("pzt = %.5f\n",pzt);
-  if(abs(pzt) < sp.q * pow(2,-2-sp.N-sp.alphalen))
+int64_t MMapIsZero(mpz_t u){
+  mpz_t left, right;
+  //left = sp.pzt * u % sp.q;
+  mpz_init(left);
+  mpz_mul(left,sp.pzt,u);
+  mpz_mod(left,left,sp.q);
+  //right = sp.q * pow(2,- alpha - beta);
+  mpz_init(right);
+  mpz_t tmp;
+  mpz_init_set_ui(tmp,pow(2,-alpha-beta));
+  mpz_mul(right,sp.q,tmp);
+
+  cout << "left = "<<left<<", right = " << right <<endl;
+  if(left < right)
 	return 1;
   return 0;
 }
 
-double MMapEnc(int64_t m, int64_t mid, int64_t setid){
-  int64_t set = 1;
+void MMapEnc(mpz_t result, int64_t m, int64_t mid, int64_t setid){
+  mpz_t set, tmpM;
+  mpz_init_set_ui(set,1);
+  mpz_init_set_ui(tmpM,m);
   for(int i=0; i<sp.setid[setid].size(); i++){
-	int64_t z = sp.setid[setid][i];
-    set = set * z;
+	mpz_t zinv;
+	mpz_init(zinv);
+    mpz_invert(zinv, sp.z[sp.setid[setid][i]], sp.p[i]); 
+    mpz_mul(set, set, zinv);
   }
-  printf("hi = %d, gi = %d, m = %d, set = %d, p = %d\n", sp.h[mid], sp.g[mid], m, set, sp.p[mid]);
-  double rest = fmod((sp.h[mid] * sp.g[mid] + m) / set, sp.p[mid]);
-  return rest;
+  int tmpR = rand() % 16 + 1;
+  mpz_t tmpRi;
+  mpz_init_set_ui(tmpRi,tmpR);
+  //result = (tmpRi * sp.g[mid] + m) * set % sp.p[mid];
+  mpz_mul(result, tmpRi, sp.g[mid]);
+  mpz_add(result, result, tmpM);
+  mpz_mul(result, result, set);
+  mpz_mod(result, result, sp.p[mid]);
 }
 
-double MMapAdd(double u1, double u2, int64_t mid){
-  double rest = fmod(u1+u2, sp.p[mid]);
-  return rest;
+void MMapAdd(mpz_t result, mpz_t u1, mpz_t u2, int64_t mid){
+  mpz_add(result, u1, u2);
+  mpz_mod(result, result, sp.p[mid]);
 }
 
-double MMapMult(double u1, double u2, int64_t mid){
-  double rest = fmod(u1*u2, sp.p[mid]);
-  return rest;
+void MMapMult(mpz_t result, mpz_t u1, mpz_t u2, int64_t mid){
+  mpz_mul(result, u1, u2);
+  mpz_mod(result, result, sp.p[mid]);
 }
--- lib/Transforms/SymObf/mmap/test.cpp
+++ lib/Transforms/SymObf/mmap/test.cpp
@@ -3,7 +3,9 @@
 int main(int argc, char** argv){
   MMapInitParam(1,1,1);
   int64_t a = atoi(argv[1]);
-  double ea = MMapEnc(a,0,0);
-  printf("%d\n",MMapIsZero(ea));
+  mpz_t em;
+  mpz_init(em);
+  MMapEnc(em,a,0,0);
+  printf("%d\n",MMapIsZero(em));
   return 0;
 }
